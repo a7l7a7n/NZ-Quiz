fffa45eae9a63d2b99065e86292ecc99cabe7548
a7l7a7n <125312711+a7l7a7n@users.noreply.github.com>
2023-04-28 15:11:16 +1200

    Initial commit

---
+++ 00_Welcome_Screen_v1.py
@@ -0,0 +1,18 @@
+"""v1_Welcome_screen
+This program is the first and hopefully the only version that welcomes the user
+for the start of the quiz
+"""
+
+
+# Allows the welcome screen to have characters around the welcome text
+def formatter(symbol, text):
+    sides = symbol * 2
+    formatter_text = f"{sides}, {text}, {sides}"
+    top_bottom = symbol * len(formatter_text)
+    return f"{top_bottom}\n{formatter_text}\n{top_bottom}"
+
+
+# Main routine
+print(formatter("🥝", "Welcome to The NZ QUIZ for Kiwis"))
+print()
+
