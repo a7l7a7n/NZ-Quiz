c6ef7bd556a6898a2473e2beb969ceacb14ae0d1
a7l7a7n <125312711+a7l7a7n@users.noreply.github.com>
2023-05-11 11:44:11 +1200

    update

--- 04_Questions_v2.py
+++ 04_Questions_v2.py
@@ -1,28 +1,30 @@
-""" Questions_v1
-Asks the user a random Question out of Maroi number 1 - 20
+""" Questions_v2
+This code is the continuation of the last version. It now asks ten questions total instead of just one
 """
 
 import random
 
+# Maroi numbers in a list
 Maroi_numbers = ["Kore", "Tahi", "Rua", "Toru", "Whā", "Rima", "Ono", "Whitu", "Waru", "Iwa",
                  "Tekau", "Tekau mā Tahi", "Tekau mā Rua", "Tekau mā Toru", "Tekau mā Whā",
                  "Tekau mā Rima", "Tekau mā Ono", "Tekau mā Whitu", "Tekau mā Waru", "Tekau mā Iwa",
                  "Rua Tekau"]
+# The English number in a list
 English_Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
                    "18", "19", "20"]
 
 # Function
+# Asks the user a question
 question = random.choice(Maroi_numbers)
 attempt = input(f"What is the English number for {question}: ")
 
+# Finds out if the user is correct or not
 answer_index = Maroi_numbers.index(question)
 answer = English_Numbers[answer_index]
 
+# Printing users results
 if attempt == answer:
     print("🥝🥝🥝Correct, Good Job!🥝🥝🥝")
 
 else:
     print("🥝🥝🥝Incorrect, Good try though!🥝🥝🥝")
-
-
-# Main Routine
